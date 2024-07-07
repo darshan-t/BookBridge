c8331deff66a62d6d995eba52d89122529480cc0
darshan-t <darshandare96@gmail.com>
2024-07-07 14:44:50 +0530

    updated file

--- src/main/java/com/example/BookBridge/entity/Book.java
+++ src/main/java/com/example/BookBridge/entity/Book.java
@@ -46,4 +46,33 @@ public class Book {
             inverseJoinColumns = {@JoinColumn(name = "publisher_id")})
     private Set<Publisher> publishers  =new HashSet<Publisher>();
 
+    public void removePublisher(Publisher publisher){
+        this.publishers.remove(publisher);
+        publisher.getBooks().remove(publisher);
+    }
+
+    public void createPublisher(Publisher publisher){
+        this.publishers.add(publisher);
+        publisher.getBooks().add(this);
+    }
+
+    public void removeCategory(Category category){
+        this.categories.remove(category);
+        category.getBooks().remove(category);
+    }
+    public void createCategory(Category category){
+        this.categories.add(category);
+        category.getBooks().add(this);
+    }
+
+    public void removeAuthor(Author author){
+        this.authors.remove(author);
+        author.getBooks().remove(author);
+    }
+
+    public void createAuthor(Author author){
+        this.authors.add(author);
+        author.getBooks().add(this);
+    }
+
 }
